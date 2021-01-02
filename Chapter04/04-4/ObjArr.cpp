fce461fa8b4b179ed6707ccf165282fdb6ee8e08
wooyoung <riverlike14@gmail.como>
2021-01-03 05:28:10 +0900

    chapter 04-4 complete

---
+++ Chapter04/04-4/ObjArr.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person
+{
+private:
+	char * name;
+	int age;
+
+public:
+	Person(char * myname, int myage)
+	{
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+
+		age = myage;
+	}
+
+	Person()
+	{
+		name = NULL;
+		age = 0;
+		cout << "called Person()" << endl;
+	}
+
+	void SetPersonInfo(char * myname, int myage)
+	{
+		name = myname;
+		age = myage;
+	}
+
+	void ShowPersonInfo() const
+	{
+		cout << "Name: " << name << ", " << "Age: " << age << endl;
+	}
+
+	~Person()
+	{
+		delete []name;
+		cout << "called destructor!" << endl;
+	}
+};
+
+int main()
+{
+	Person parr[3];
+	char namestr[100];
+	char * strptr;
+	int age;
+	int len;
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Name: "; cin >> namestr;
+		cout << "Age: "; cin >> age;
+
+		len = strlen(namestr) + 1;
+		strptr = new char[len];
+		strcpy(strptr, namestr);
+
+		parr[i].SetPersonInfo(strptr, age);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		parr[i].ShowPersonInfo();
+	}
+
+	return 0;
+}
