1112712ca9b7758c8a2bb9a4ab9139e9ead7cf73
wooyoung <riverlike14@gmail.com>
2021-01-15 13:01:22 +0900

    OOP project step3 complete

---
+++ OOP_Project/step3/Account.h
@@ -0,0 +1,27 @@
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+const int NAME_LEN = 30;
+
+enum {
+	CREATE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT
+};
+
+class Account
+{
+private:
+	int accountID;
+	char * name;
+	int balance;
+
+public:
+	Account(int newID, char * newName, int money);
+	Account(const Account& ref);
+	void Deposit(int money);
+	bool Withdraw(int money);
+	void ShowInfo() const;
+	bool AccountIDCheck(int accountID);
+	~Account();
+};
+
+#endif
