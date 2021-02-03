91116937a93bae1411a224b9973dc91a0a341ab8
wooyoung <riverlike14@gmail.como>
2021-02-03 10:18:28 +0900

    chapter 08-1 complete

---
+++ Chapter08/08-1/EmployeeManager2.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Employee
+{
+private:
+	char name[100];
+
+public:
+	Employee(char* name)
+	{
+		strcpy(this->name, name);
+	}
+
+	void ShowYourName() const
+	{
+		cout << "Name: " << name << endl;
+	}
+};
+
+class PermanentWorker : public Employee
+{
+private:
+	int salary;
+
+public:
+	PermanentWorker(char* name, int money)
+		: Employee(name), salary(money)
+	{ }
+
+	int GetPay() const
+	{
+		return salary;
+	}
+
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout << "Salary: " << salary << endl;
+	}
+};
+
+class EmployeeHandler
+{
+private:
+	Employee* empList[50];
+	int empNum;
+
+public:
+	EmployeeHandler()
+		: empNum(0)
+	{  }
+
+	void AddEmployee(Employee* emp)
+	{
+		empList[empNum++] = emp;
+	}
+
+	void ShowAllSalaryInfo() const
+	{
+		for (int i = 0; i < empNum; i++)
+		{
+			// empList[i]->ShowSalaryInfo();
+			cout << endl;
+		}
+	}
+
+	void ShowTotalSalary() const
+	{
+		int sum = 0;
+		for (int i = 0; i < empNum; i++)
+			;
+			// sum += empList[i]->GetPay();
+		cout << "Salary sum: " << sum << endl;
+	}
+
+	~EmployeeHandler()
+	{
+		for (int i = 0; i < empNum; i++)
+			delete empList[i];
+	}
+};
+
+int main()
+{
+	EmployeeHandler handler;
+
+	handler.AddEmployee(new PermanentWorker("KIM", 1000));
+	handler.AddEmployee(new PermanentWorker("LEE", 1500));
+	handler.AddEmployee(new PermanentWorker("JUN", 2000));
+
+	handler.ShowAllSalaryInfo();
+
+	handler.ShowTotalSalary();
+
+	return 0;
+}
